0de2310e0fcdd7af5a831bfc4116af7ac4426b6e
widmann <widmann@uni-leipzig.de>
2010-02-11 10:21:05 +0000

    Version number

--- eegplugin_sphspline.m
+++ eegplugin_sphspline.m
@@ -33,7 +33,7 @@
 
 function vers = eegplugin_sphspline(fig, trystrs, catchstrs)
 
-    vers = 'sphspline0.11';
+    vers = 'sphspline0.2beta';
     if nargin < 3
         error('eegplugin_sphspline requires 3 arguments');
     end
